6c8e5f598936c3c47932829ef86f7049a5a9ae35
Eduardo Foncesca Finardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2024-05-29 09:43:24 -0300

    Update BeachReport.sql

--- BeachReport.sql
+++ BeachReport.sql
@@ -3,14 +3,14 @@ DROP TABLE T_BR_ENDERECO_USUARIO;
 DROP TABLE T_BR_TELEFONE_USUARIO;
 DROP TABLE T_BR_NIVEL_CONFIABILIDADE;
 DROP TABLE T_BR_RELATO;
-DROP TABLE T_BR_EMAIL_CONTATO;
+DROP TABLE T_BR_EMAIL_USUARIO;
 DROP TABLE T_BR_USUARIO;
 DROP TABLE T_BR_LOG_ERROS;
 
 DROP SEQUENCE SEQ_USUARIO;
 DROP SEQUENCE SEQ_ENDERECO_USUARIO;
 DROP SEQUENCE SEQ_TELEFONE_USUARIO;
-DROP SEQUENCE SEQ_EMAIL_CONTATO_2;
+DROP SEQUENCE SEQ_EMAIL_USUARIO;
 DROP SEQUENCE SEQ_NIVEL_CONFIABILIDADE;
 DROP SEQUENCE SEQ_RELATO;
 DROP SEQUENCE SEQ_LOG_ERROS;
@@ -62,15 +62,15 @@ CREATE SEQUENCE SEQ_TELEFONE_USUARIO
   MAXVALUE 1000
   NOCYCLE;
 ----------------------------------------------------------
-CREATE TABLE T_BR_EMAIL_CONTATO(
+CREATE TABLE T_BR_EMAIL_USUARIO(
     id_email_contato NUMBER,
     varchar_email VARCHAR(80),
     id_usuario NUMBER,
-    CONSTRAINT T_BR_EMAIL_CONTATO_PK PRIMARY KEY (id_email_contato),
-    CONSTRAINT T_BR_EMAIL_CONTATO_USUARIO_FK FOREIGN KEY (id_usuario) REFERENCES T_BR_USUARIO(id_usuario)   
+    CONSTRAINT T_BR_EMAIL_USUARIO_PK PRIMARY KEY (id_email_contato),
+    CONSTRAINT T_BR_EMAIL_USUARIO_USUARIO_FK FOREIGN KEY (id_usuario) REFERENCES T_BR_USUARIO(id_usuario)   
 );
 
-CREATE SEQUENCE SEQ_EMAIL_CONTATO_2
+CREATE SEQUENCE SEQ_EMAIL_USUARIO
   START WITH 1
   INCREMENT BY 1
   MINVALUE 0
@@ -98,6 +98,9 @@ CREATE TABLE T_BR_RELATO(
     ds_relato VARCHAR2(4000),
     nr_latitude NUMBER,
     nr_longitude NUMBER,
+    boolean_praia_suja NUMBER(1), 
+    boolean_envolve_animais NUMBER(1),
+    dt_hr_relato DATE,
     id_usuario NUMBER,
     CONSTRAINT T_BR_RELATO_PK PRIMARY KEY (id_relato),
     CONSTRAINT T_BR_RELATO_USUARIO_FK FOREIGN KEY (id_usuario) REFERENCES T_BR_USUARIO(id_usuario)   
@@ -129,29 +132,43 @@ CREATE SEQUENCE SEQ_LOG_ERROS
   
 ----------------------------------------------------------------------------------
 --------------------------PROCEDURE - CARGA DE DADOS------------------------------
----T_BR_USUARIO
-CREATE OR REPLACE PROCEDURE SP_INSERIR_USUARIO (
-    p_nr_cpf NUMBER,
-    p_varchar_senha VARCHAR
-) IS
-    v_id_usuario NUMBER;
+---T_BR_LOG_ERRO
+CREATE OR REPLACE PROCEDURE PRC_INSERIR_LOG_ERRO (
+    p_nome_procedure IN VARCHAR2,
+    p_nome_usuario IN VARCHAR2,
+    p_codigo_erro IN NUMBER,
+    p_mensagem_erro IN VARCHAR2
+) AS
 BEGIN
-    SELECT SEQ_USUARIO.NEXTVAL INTO v_id_usuario FROM DUAL;
-    INSERT INTO T_BR_USUARIO (id_usuario, nr_cpf, varchar_senha)
-    VALUES (v_id_usuario, p_nr_cpf, p_varchar_senha);
+    INSERT INTO T_BR_LOG_ERROS (
+        id_log, nome_procedure, nome_usuario, data_ocorrencia, codigo_erro, mensagem_erro
+    ) VALUES (
+        SEQ_LOG_ERROS.NEXTVAL, p_nome_procedure, p_nome_usuario, SYSDATE, p_codigo_erro, p_mensagem_erro
+    );
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL; -- Evitar loop infinito em caso de erro na inserção do log
+END;
+---
+
+---T_BR_USUARIO
+CREATE OR REPLACE PROCEDURE PRC_INSERIR_USUARIO (
+    p_nr_cpf IN NUMBER,
+    p_varchar_senha IN VARCHAR2
+) AS
+BEGIN
+    INSERT INTO T_BR_USUARIO (
+        id_usuario, nr_cpf, varchar_senha
+    ) VALUES (
+        SEQ_USUARIO.NEXTVAL, p_nr_cpf, p_varchar_senha
+    );
+    COMMIT;
 EXCEPTION
     WHEN DUP_VAL_ON_INDEX THEN
-        INSERT INTO T_BR_LOG_ERROS (id_log, nome_procedure, nome_usuario, data_ocorrencia, codigo_erro, mensagem_erro)
-        VALUES (SEQ_LOG_ERROS.NEXTVAL, 'SP_INSERIR_USUARIO', USER, SYSDATE, SQLCODE, SQLERRM);
-    WHEN VALUE_ERROR THEN
-        INSERT INTO T_BR_LOG_ERROS (id_log, nome_procedure, nome_usuario, data_ocorrencia, codigo_erro, mensagem_erro)
-        VALUES (SEQ_LOG_ERROS.NEXTVAL, 'SP_INSERIR_USUARIO', USER, SYSDATE, SQLCODE, SQLERRM);
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_USUARIO', USER, SQLCODE, SQLERRM);
+        RAISE_APPLICATION_ERROR(-20001, 'Erro: CPF duplicado.');
     WHEN OTHERS THEN
-        INSERT INTO T_BR_LOG_ERROS (id_log, nome_procedure, nome_usuario, data_ocorrencia, codigo_erro, mensagem_erro)
-        VALUES (SEQ_LOG_ERROS.NEXTVAL, 'SP_INSERIR_USUARIO', USER, SYSDATE, SQLCODE, SQLERRM);
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_USUARIO', USER, SQLCODE, SQLERRM);
+        RAISE;
 END;
-
-EXEC SP_INSERIR_USUARIO(p_nr_cpf => 12345678901, p_varchar_senha => 'senha123');
----
-
----
