706f1e27437cd1718c592b2b32ea0085ad154f19
Eduardo Foncesca Finardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2024-05-31 19:36:09 -0300

    Update BeachReport.sql

--- BeachReport.sql
+++ BeachReport.sql
@@ -14,7 +14,6 @@ DROP SEQUENCE SEQ_EMAIL_USUARIO;
 DROP SEQUENCE SEQ_NIVEL_CONFIABILIDADE;
 DROP SEQUENCE SEQ_RELATO;
 DROP SEQUENCE SEQ_LOG_ERROS;
-
 ------------------CREATE---------------------------------
 ---------------------------------------------------------
 CREATE TABLE T_BR_USUARIO(
@@ -101,6 +100,7 @@ CREATE TABLE T_BR_RELATO(
     boolean_praia_suja NUMBER(1), 
     boolean_envolve_animais NUMBER(1),
     dt_hr_relato DATE,
+    nr_likes NUMBER,
     id_usuario NUMBER,
     CONSTRAINT T_BR_RELATO_PK PRIMARY KEY (id_relato),
     CONSTRAINT T_BR_RELATO_USUARIO_FK FOREIGN KEY (id_usuario) REFERENCES T_BR_USUARIO(id_usuario)   
@@ -195,12 +195,9 @@ END;
 ----
 ---- teste EXCEPTION
 BEGIN
-    -- Primeiro, insere um usuário válido
     PRC_INSERIR_USUARIO(p_nr_cpf => 12345678901, p_varchar_senha => 'senha123');
-    -- Tenta inserir o mesmo CPF novamente para gerar um erro de duplicidade
     PRC_INSERIR_USUARIO(p_nr_cpf => 12345678901, p_varchar_senha => 'senha124');
 END;
-
 ---- TESTE EXTRA
 SELECT object_name, status
 FROM user_objects
@@ -241,11 +238,9 @@ END;
 ----
 ---- teste EXCEPTION
 BEGIN
-    -- Tenta inserir um endereço para um usuário que não existe
     PRC_INSERIR_ENDERECO_USUARIO(p_nr_cep => 12345678, p_id_usuario => 9999); -- id_usuario 9999 não existe
 END;
 ----
-
 select * from T_BR_ENDERECO_USUARIO;
 select * from T_BR_LOG_ERROS;
 ----
@@ -282,10 +277,317 @@ END;
 BEGIN
     PRC_INSERIR_TELEFONE_USUARIO(p_nr_ddd => 11, p_nr_telefone => 987654321, p_id_usuario => 9999); -- id_usuario 9999 não existe
 END;
-
+--- teste EXTRA
+SELECT object_name, status
+FROM user_objects
+WHERE object_type = 'PROCEDURE'
+AND object_name = 'PRC_INSERIR_LOG_ERRO';
+----
 select * from T_BR_TELEFONE_USUARIO;
 select * from T_BR_LOG_ERROS;
 ----
 
 
 
+---- T_BR_EMAIL_USUSARIO
+CREATE OR REPLACE PROCEDURE PRC_INSERIR_EMAIL_USUARIO (
+    p_varchar_email IN VARCHAR2,
+    p_id_usuario IN NUMBER
+) AS
+BEGIN
+    INSERT INTO T_BR_EMAIL_USUARIO (
+        id_email_contato, varchar_email, id_usuario
+    ) VALUES (
+        SEQ_EMAIL_USUARIO.NEXTVAL, p_varchar_email, p_id_usuario
+    );
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_EMAIL_USUARIO', USER, SQLCODE, SQLERRM);
+        RAISE_APPLICATION_ERROR(-20002, 'Erro: Usuário não encontrado.');
+    WHEN OTHERS THEN
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_EMAIL_USUARIO', USER, SQLCODE, SQLERRM);
+        RAISE;
+END;
+
+----
+---- teste válido
+BEGIN
+    PRC_INSERIR_EMAIL_USUARIO(p_varchar_email => 'usuario@exemplo.com', p_id_usuario => 1);
+END;
+
+----
+---- teste EXCEPTION
+BEGIN
+    PRC_INSERIR_EMAIL_USUARIO(p_varchar_email => 'usuario@exemplo.com', p_id_usuario => 9999); -- id_usuario 9999 não existe
+END;
+---- teste EXTRA
+
+----
+
+select * from T_BR_EMAIL_USUARIO;
+select * from T_BR_LOG_ERROS;
+----
+
+
+
+---- T_BR_NIVEL_CONFIABILIDADE
+CREATE OR REPLACE PROCEDURE PRC_INSERIR_NIVEL_CONFIABILIDADE (
+    p_numeric_nivel_confiabilidade IN NUMBER,
+    p_id_usuario IN NUMBER
+) AS
+BEGIN
+    INSERT INTO T_BR_NIVEL_CONFIABILIDADE (
+        id_nivel_confiabilidade, numeric_nivel_confiabilidade, id_usuario
+    ) VALUES (
+        SEQ_NIVEL_CONFIABILIDADE.NEXTVAL, p_numeric_nivel_confiabilidade, p_id_usuario
+    );
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_NIVEL_CONFIABILIDADE', USER, SQLCODE, SQLERRM);
+        RAISE_APPLICATION_ERROR(-20002, 'Erro: Usuário não encontrado.');
+    WHEN OTHERS THEN
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_NIVEL_CONFIABILIDADE', USER, SQLCODE, SQLERRM);
+        RAISE;
+END;
+----
+---- teste válido
+BEGIN
+    PRC_INSERIR_NIVEL_CONFIABILIDADE(p_numeric_nivel_confiabilidade => 5, p_id_usuario => 1);
+END;
+----
+---- teste EXCEPTION
+BEGIN
+    PRC_INSERIR_NIVEL_CONFIABILIDADE(p_numeric_nivel_confiabilidade => 5, p_id_usuario => 9999); -- id_usuario 9999 não existe
+END;
+---- teste EXTRA
+
+----
+select * from T_BR_NIVEL_CONFIABILIDADE;
+select * from T_BR_LOG_ERROS;
+----
+
+
+
+---- T_BR_RELATO
+CREATE OR REPLACE PROCEDURE PRC_INSERIR_RELATO (
+    p_blob_foto IN BLOB,
+    p_ds_relato IN VARCHAR2,
+    p_nr_latitude IN NUMBER,
+    p_nr_longitude IN NUMBER,
+    p_boolean_praia_suja IN NUMBER,
+    p_boolean_envolve_animais IN NUMBER,
+    p_id_usuario IN NUMBER,
+    p_nr_likes IN NUMBER,
+    p_dt_hr_relato IN DATE
+) AS
+BEGIN
+    INSERT INTO T_BR_RELATO (
+        id_relato, blob_foto, ds_relato, nr_latitude, nr_longitude, boolean_praia_suja, boolean_envolve_animais, id_usuario, nr_likes, dt_hr_relato
+    ) VALUES (
+        SEQ_RELATO.NEXTVAL, p_blob_foto, p_ds_relato, p_nr_latitude, p_nr_longitude, p_boolean_praia_suja, p_boolean_envolve_animais, p_id_usuario,p_nr_likes, p_dt_hr_relato
+    );
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_RELATO', USER, SQLCODE, SQLERRM);
+        RAISE_APPLICATION_ERROR(-20002, 'Erro: Usuário não encontrado.');
+    WHEN OTHERS THEN
+        PRC_INSERIR_LOG_ERRO('PRC_INSERIR_RELATO', USER, SQLCODE, SQLERRM);
+        RAISE;
+END;
+----
+---- teste válido
+DECLARE
+    v_blob_foto BLOB;
+BEGIN
+    DBMS_LOB.CREATETEMPORARY(v_blob_foto, TRUE);
+
+    PRC_INSERIR_RELATO(
+        p_blob_foto => v_blob_foto,
+        p_ds_relato => 'Relato de exemplo',
+        p_nr_latitude => -23.55052,
+        p_nr_longitude => -46.633308,
+        p_boolean_praia_suja => 1,
+        p_boolean_envolve_animais => 0,
+        p_id_usuario => 1,
+        p_nr_likes => 10,
+        p_dt_hr_relato => SYSDATE
+    );
+
+    DBMS_LOB.FREETEMPORARY(v_blob_foto);
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_LOB.FREETEMPORARY(v_blob_foto);
+        RAISE;
+END;
+----
+---- teste EXCEPTION
+DECLARE
+    v_blob_foto BLOB;
+BEGIN
+    DBMS_LOB.CREATETEMPORARY(v_blob_foto, TRUE);
+
+    PRC_INSERIR_RELATO(
+        p_blob_foto => v_blob_foto,
+        p_ds_relato => 'Relato de exemplo',
+        p_nr_latitude => -23.55052,
+        p_nr_longitude => -46.633308,
+        p_boolean_praia_suja => 1,
+        p_boolean_envolve_animais => 0,
+        p_id_usuario => 9999, 
+        p_nr_likes => 10,
+        p_dt_hr_relato => SYSDATE
+    );
+
+    DBMS_LOB.FREETEMPORARY(v_blob_foto);
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_LOB.FREETEMPORARY(v_blob_foto);
+        RAISE;
+END;
+---- teste EXTRA
+
+----
+select * from T_BR_RELATO;
+select * from T_BR_LOG_ERROS;
+
+--------------------------------------------------------------------------------
+--------------------------BLOCOS ANÔNIMOS - CURSOR------------------------------
+set verify off
+set serveroutput on
+
+----
+DECLARE
+    CURSOR c_relato IS
+        SELECT * FROM T_BR_RELATO;
+    v_relato c_relato%ROWTYPE;
+BEGIN
+    OPEN c_relato;
+    LOOP
+        FETCH c_relato INTO v_relato;
+        EXIT WHEN c_relato%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('ID: ' || v_relato.id_relato || ', Descrição: ' || v_relato.ds_relato || ', Latitude: ' || v_relato.nr_latitude || ', Longitude: ' || v_relato.nr_longitude);
+    END LOOP;
+    CLOSE c_relato;
+END;
+----
+
+
+
+----
+DECLARE
+    CURSOR c_relato IS
+        SELECT boolean_praia_suja FROM T_BR_RELATO;
+    v_boolean_praia_suja T_BR_RELATO.boolean_praia_suja%TYPE;
+    v_total_praia_suja NUMBER := 0;
+    v_total_praia_nao_suja NUMBER := 0;
+BEGIN
+    OPEN c_relato;
+    LOOP
+        FETCH c_relato INTO v_boolean_praia_suja;
+        EXIT WHEN c_relato%NOTFOUND;
+        IF v_boolean_praia_suja = 1 THEN
+            v_total_praia_suja := v_total_praia_suja + 1;
+        ELSE
+            v_total_praia_nao_suja := v_total_praia_nao_suja + 1;
+        END IF;
+    END LOOP;
+    CLOSE c_relato;
+    DBMS_OUTPUT.PUT_LINE('Total Praia Suja: ' || v_total_praia_suja);
+    DBMS_OUTPUT.PUT_LINE('Total Praia Não Suja: ' || v_total_praia_nao_suja);
+END;
+----
+
+
+
+----
+DECLARE
+    CURSOR c_relato IS
+        SELECT boolean_envolve_animais FROM T_BR_RELATO;
+    v_boolean_envolve_animais T_BR_RELATO.boolean_envolve_animais%TYPE;
+    v_total_com_animais NUMBER := 0;
+    v_total_sem_animais NUMBER := 0;
+BEGIN
+    OPEN c_relato;
+    LOOP
+        FETCH c_relato INTO v_boolean_envolve_animais;
+        EXIT WHEN c_relato%NOTFOUND;
+        IF v_boolean_envolve_animais = 1 THEN
+            v_total_com_animais := v_total_com_animais + 1;
+        ELSE
+            v_total_sem_animais := v_total_sem_animais + 1;
+        END IF;
+    END LOOP;
+    CLOSE c_relato;
+    DBMS_OUTPUT.PUT_LINE('Total Casos com Animais: ' || v_total_com_animais);
+    DBMS_OUTPUT.PUT_LINE('Total Casos sem Animais: ' || v_total_sem_animais);
+END;
+----
+
+
+----
+DECLARE
+    CURSOR c_relato IS
+        SELECT id_relato, blob_foto, ds_relato, nr_latitude, nr_longitude, boolean_praia_suja, boolean_envolve_animais, id_usuario, dt_hr_relato
+        FROM T_BR_RELATO
+        WHERE nr_latitude BETWEEN -23.6 AND -23.5
+        ORDER BY nr_latitude;
+    
+    v_relato c_relato%ROWTYPE;
+    v_total_praia_suja NUMBER := 0;
+    v_total_praia_nao_suja NUMBER := 0;
+    v_total_com_animais NUMBER := 0;
+    v_total_sem_animais NUMBER := 0;
+    v_current_latitude_interval NUMBER := NULL;
+    v_interval_size NUMBER := 0.01; 
+BEGIN
+    OPEN c_relato;
+    LOOP
+        FETCH c_relato INTO v_relato;
+        EXIT WHEN c_relato%NOTFOUND;
+
+        IF v_current_latitude_interval IS NULL OR v_relato.nr_latitude >= v_current_latitude_interval + v_interval_size THEN
+            IF v_current_latitude_interval IS NOT NULL THEN
+                DBMS_OUTPUT.PUT_LINE('--- Fim do Intervalo de Latitude: ' || v_current_latitude_interval || ' a ' || (v_current_latitude_interval + v_interval_size));
+                DBMS_OUTPUT.PUT_LINE('Total Praia Suja: ' || v_total_praia_suja);
+                DBMS_OUTPUT.PUT_LINE('Total Praia Não Suja: ' || v_total_praia_nao_suja);
+                DBMS_OUTPUT.PUT_LINE('Total Casos com Animais: ' || v_total_com_animais);
+                DBMS_OUTPUT.PUT_LINE('Total Casos sem Animais: ' || v_total_sem_animais);
+                DBMS_OUTPUT.PUT_LINE('----------------------------------------');
+                v_total_praia_suja := 0;
+                v_total_praia_nao_suja := 0;
+                v_total_com_animais := 0;
+                v_total_sem_animais := 0;
+            END IF;
+            v_current_latitude_interval := FLOOR(v_relato.nr_latitude / v_interval_size) * v_interval_size;
+            DBMS_OUTPUT.PUT_LINE('--- Início do Intervalo de Latitude: ' || v_current_latitude_interval || ' a ' || (v_current_latitude_interval + v_interval_size));
+        END IF;
+
+        DBMS_OUTPUT.PUT_LINE('ID: ' || v_relato.id_relato || ', Descrição: ' || v_relato.ds_relato || ', Latitude: ' || v_relato.nr_latitude || ', Longitude: ' || v_relato.nr_longitude || ', Praia Suja: ' || v_relato.boolean_praia_suja || ', Envolve Animais: ' || v_relato.boolean_envolve_animais || ', Usuário: ' || v_relato.id_usuario || ', Data: ' || v_relato.dt_hr_relato);
+
+        IF v_relato.boolean_praia_suja = 1 THEN
+            v_total_praia_suja := v_total_praia_suja + 1;
+        ELSE
+            v_total_praia_nao_suja := v_total_praia_nao_suja + 1;
+        END IF;
+
+        IF v_relato.boolean_envolve_animais = 1 THEN
+            v_total_com_animais := v_total_com_animais + 1;
+        ELSE
+            v_total_sem_animais := v_total_sem_animais + 1;
+        END IF;
+    END LOOP;
+
+    IF v_current_latitude_interval IS NOT NULL THEN
+        DBMS_OUTPUT.PUT_LINE('--- Fim do Intervalo de Latitude: ' || v_current_latitude_interval || ' a ' || (v_current_latitude_interval + v_interval_size));
+        DBMS_OUTPUT.PUT_LINE('Total Praia Suja: ' || v_total_praia_suja);
+        DBMS_OUTPUT.PUT_LINE('Total Praia Não Suja: ' || v_total_praia_nao_suja);
+        DBMS_OUTPUT.PUT_LINE('Total Casos com Animais: ' || v_total_com_animais);
+        DBMS_OUTPUT.PUT_LINE('Total Casos sem Animais: ' || v_total_sem_animais);
+        DBMS_OUTPUT.PUT_LINE('----------------------------------------');
+    END IF;
+
+    CLOSE c_relato;
+END;
